42ec957a3906010d4668f4eed8dd96c57e6b1aa8
atomgx7 <atomgx7@gmail.com>
2023-11-14 14:21:23 -0600

    Agregando GET para obtener usuarios y agregado el use en caso de que no sea correcta la request

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require('express');
 const database = require('./dbconnection.js');
 const app = express();
 
+app.disable('x-powered-by');
+
+
+//Cuando obtenga un get en la ruta
+app.get('/getusers', (req, res) => {
+    let users = database.execute(`SELECT user FROM users;`);
+    
+});
+
+
+app.use((req, res) => {
+    res.send('<h1> 404 NOT FOUND <h1>');
+})
