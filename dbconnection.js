37a3e8802c6d48568efa4840e0865b673117b296
atomgx7 <atomgx7@gmail.com>
2023-11-14 12:00:35 -0600

    Agregado try catch a dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,12 +1,18 @@
 const mysql2 = require('mysql2');
 require('dotenv').config();
 
-const Connection = mysql2.createConnection({
-    host: process.env.HOST_DATABASE,
-    port: process.env.PORT,
-    user: process.env.USER_DATABASE,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD_DATABASE
-});
+
+try {
+    const Connection = mysql2.createConnection({
+        host: process.env.HOST_DATABASE,
+        port: process.env.PORT,
+        user: process.env.USER_DATABASE,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD_DATABASE
+    });
+} catch (error) {
+    console.log(`Error al conectar a la db: \n ${error}`);
+}
+
 
 module.exports = { Connection };
