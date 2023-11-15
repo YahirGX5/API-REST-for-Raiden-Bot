84b99b06b7ecc40273ecdcb473413558edc56ed6
atomgx7 <atomgx7@gmail.com>
2023-11-15 12:06:01 -0600

    Cambios menores, como un try catch, sintaxis, etc

--- dbconnection.js
+++ dbconnection.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 
 
 try {
-    const Connection = mysql2.createConnection({
+    const connection = mysql2.createConnection({
         host: process.env.HOST_DATABASE,
         port: process.env.PORT,
         user: process.env.USER_DATABASE,
         database: process.env.DATABASE,
         password: process.env.PASSWORD_DATABASE
     });
+
 } catch (error) {
+
     console.log(`Error al conectar a la db: \n ${error}`);
+
 }
 
 
-module.exports = { Connection };
+module.exports = { connection }
--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const app = express();
 app.disable('x-powered-by');
 
 
-//Cuando obtenga un get en la ruta
-app.get('/getusers', (req, res) => {
-    let users = database.execute(`SELECT user FROM users;`);
+//Cuando obtenga un get en la ruta '/get-users'
+app.get('/get-users', (req, res) => {
+    try {
+        let users = database.execute(`SELECT user FROM users;`);
+    } catch (error) {
+        res.status(500).send('<h1> Internal Server Error <h1>');
+    }
     
 });
 
 
 app.use((req, res) => {
     res.send('<h1> 404 NOT FOUND <h1>');
-})
+});
